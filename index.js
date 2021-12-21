d900acbf93bf7ab30dc100db4467718af53e8ac5
Rodrigo Guedelho <rodrigohmjj97@hotmail.com>
2021-12-21 16:10:03 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,68 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+  const tela = document.querySelector('#telaDesenho');
+  const btnBorracha = document.querySelector("#btnBorracha")
+  const contexto = tela.getContext('2d');
+
+  tela.width = 700;
+  tela.height = 500;
+
+  contexto.lineWidth = 7;
+  contexto.strokeStyle = "red";
+
+  const pincel = {
+    ativo: false,
+    movendo : false,
+    posicao: {x: 0, y: 0},
+    posicaoAnterior : null,
+    borracha : false
+  }
+
+  const desenhaLinha = (linha) => {
+    contexto.beginPath();
+    contexto.moveTo(linha.posicaoAnterior.x, linha.posicaoAnterior.y);
+    contexto.lineTo(linha.posicao.x, linha.posicao.y);
+    contexto.stroke();
+
+  }
+
+  const ativarOuDesativarBorracha = () => {
+    pincel.borracha = !pincel.borracha;
+    if (pincel.borracha) {
+      contexto.lineWidth = 15;
+      contexto.strokeStyle = "white";
+    } else {
+      contexto.lineWidth = 7;
+      contexto.strokeStyle = "red";
+    }
+  }
+
+  tela.onmousedown = (evento) => {
+    pincel.ativo = true;
+  }
+
+  tela.onmouseup = (evento) => {
+    pincel.ativo = false;
+  }
+
+  tela.onmousemove = (evento) => {
+    pincel.posicao.x = evento.clientX;
+    pincel.posicao.y = evento.clientY;
+    pincel.movendo = true;
+  }
+
+  btnBorracha.onclick = (evento) => {
+    ativarOuDesativarBorracha();
+  }
+
+  const cliclo = () => {
+    if (pincel.ativo && pincel.movendo && pincel.posicaoAnterior) {
+      desenhaLinha({posicao : pincel.posicao, posicaoAnterior : pincel.posicaoAnterior});
+      pincel.movendo = false;
+    }
+
+    pincel.posicaoAnterior = {x : pincel.posicao.x, y : pincel.posicao.y};
+    setTimeout(cliclo, 10);
+  }
+
+  cliclo();
+})
